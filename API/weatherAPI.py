35c9c2bfaf7b590acf5207ed4f32b6824612bd83
Kwespo <pikej1088@gmail.com>
2024-02-21 11:19:37 +0000

    Finished weather API. Things are done.

--- API/weatherAPI.py
+++ API/weatherAPI.py
@@ -1,2 +1,36 @@
+import math
 import requests
 
+DbugMode = False
+
+# print(response['main']['temp'] - 273.15)
+
+with open('API/keyWeather.txt', 'r') as file:
+    key = file.read()
+
+def getWeather(city):
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}'
+    response = requests.get(url).json()
+    if DbugMode:
+        print(response)
+    getValue(response)
+
+def getLocation():
+    location = input('Enter the city: ')
+    getWeather(location)
+
+def getValue(response):
+    print(response.keys())
+    value = input('What value do you want to get? ')
+    try:
+        print(response[value][0].keys())
+    except:
+        print(response[value].keys())
+    data = input(f'What data do you want to get from {value}? ')
+    try:
+        print(response[value][0][data])
+    except:
+        print(response[value][data])
+
+if __name__ == '__main__':
+    getLocation()
